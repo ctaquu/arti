f30ae777132fc1db76867759e0aafca00c302377
damjan <ctaquu@gmail.com>
2019-12-29 23:16:37 +0100

    fix(tests): comments cleanup

--- tests/setup.js
+++ tests/setup.js
@@ -1,19 +1,7 @@
-// const constants = require ('../common/constants');
-// const utils = require('../common/utils')
-// const models = require('../database/models/index');
-// const moment = require('moment');
-// const sequelize = require('../database/models/index').sequelize;
 const request = require('supertest');
 const app = require('../common/server').app;
 
 module.exports = {
-    initialize: () => {
-        console.info('initialize');
-    },
-    destroy: () => {
-        console.info('destroy');
-        // sequelize.sync({force: true});
-    },
     token: () => {
         return new Promise((async resolve => {
 
